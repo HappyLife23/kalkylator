265d539db2db74a9258ed8977e2ee877e442f034
David Heidari <davidheidari2@gmail.com>
2024-01-06 12:07:34 +0100

    add html and css

---
+++ js/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let id;
+id: 'Hello';
+id: 55;
+console.log(55);
+
+
+// Skapa ett användargränssnitt för bolånekalkylatorn där användaren kan ange lånets storlek, räntesats och återbetalningstid.
