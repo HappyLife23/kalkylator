1d47d009f51bb22eb4439aac5b6257e150cbde34
David Heidari <davidheidari2@gmail.com>
2024-01-07 17:34:04 +0100

    add css, htnl and js

---
+++ ts/index.ts
@@ -0,0 +1,27 @@
+ const loanAmount = document.getElementById('loan-amount') as HTMLInputElement;
+const interestRate = document.getElementById('interest-rate') as HTMLInputElement;
+const repaymentPeriod = document.getElementById('repayment-period') as HTMLInputElement;
+const resultBtn = document.getElementById('result-btn') as HTMLButtonElement;
+
+
+
+type nums = { 
+    
+    M: Number;
+    P: Number;
+    R: Number;
+    I: number;
+}
+
+resultBtn.addEventListener('click', () => {
+    console.log('hej')
+})
+
+
+
+
+
+
+
+
+
