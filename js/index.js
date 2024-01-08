00041a73f4aa736d505bec38f90f3d7dbd2c77ab
David Heidari <davidheidari2@gmail.com>
2024-01-08 18:35:45 +0100

    add ts

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
 "use strict";
-let id;
-id: 'Hello';
-id: 55;
-console.log(55);
+const contaiiner = document.querySelector('.container');
+const loanAmountInput = document.getElementById('loan-amount');
+const interestRateInput = document.getElementById('interest-rate');
+const repaymentPeriodInput = document.getElementById('repayment-period');
+const resultBtn = document.getElementById('result-btn');
+function calculate() {
+    const p = parseInt(loanAmountInput.value);
+    const r = parseInt(interestRateInput.value) / 1200;
+    const n = parseInt(repaymentPeriodInput.value) * 12;
+    const nominator = r * (1 + r) ** n;
+    const denominator = (1 + r) ** n - 1;
+    const M = Math.round(p * (nominator / denominator));
+    const result = document.createElement('div');
+    result.innerHTML = `
+        <p>Loan amount:$${p}</p>
+        <p>Interest rate: ${r}%</p>
+        <p>Repayment period:${n} month</p>
+        <p>Monthly payment: ${M}$</p>
 
-
-// Skapa ett användargränssnitt för bolånekalkylatorn där användaren kan ange lånets storlek, räntesats och återbetalningstid.
+    `;
+    contaiiner.appendChild(result);
+}
+resultBtn.addEventListener('click', calculate);
--- ts/index.ts
+++ ts/index.ts
@@ -1,21 +1,48 @@
- const loanAmount = document.getElementById('loan-amount') as HTMLInputElement;
-const interestRate = document.getElementById('interest-rate') as HTMLInputElement;
-const repaymentPeriod = document.getElementById('repayment-period') as HTMLInputElement;
+const contaiiner = document.querySelector('.container') as HTMLDivElement;
+const loanAmountInput = document.getElementById('loan-amount') as HTMLInputElement;
+const interestRateInput = document.getElementById('interest-rate') as HTMLInputElement;
+const repaymentPeriodInput = document.getElementById('repayment-period') as HTMLInputElement;
 const resultBtn = document.getElementById('result-btn') as HTMLButtonElement;
 
 
+function calculate() {
+    const p = parseInt(loanAmountInput.value);
+    const r = parseInt(interestRateInput.value) / 1200;
+    const n = parseInt(repaymentPeriodInput.value) * 12;
 
-type nums = { 
+
+    const nominator = r * (1 + r) ** n
+    const denominator = (1 + r) ** n -1
+    const M = Math.round( p * (nominator / denominator))
     
-    M: Number;
-    P: Number;
-    R: Number;
-    I: number;
+    
+    const result = document.createElement('div')
+    result.innerHTML = `
+        <p>Loan amount:$${p}</p>
+        <p>Interest rate: ${r}%</p>
+        <p>Repayment period:${n} month</p>
+        <p>Monthly payment: ${M}$</p>
+
+    `
+    
+
+
+    contaiiner.appendChild(result)
 }
 
-resultBtn.addEventListener('click', () => {
-    console.log('hej')
-})
+resultBtn.addEventListener('click', calculate);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
